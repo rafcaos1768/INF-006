702e30828fd0421d9eb72b87dd7faf4fc130f3b2
Rafael Silva <160551377+rafcaos1768@users.noreply.github.com>
2025-10-13 21:34:30 -0300

    Create atv3.c

---
+++ CodeForces/atv3.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h> 
+
+bool separeteInput(){
+  part = strtok(birth, "/");
+    dd = atoi(part);
+
+    part = strtok(NULL, "/");
+    mm = atoi(part);
+
+    part = strtok(NULL, "/");
+    aa = atoi(part);
+}
+
+int main()
+{   
+    char *entrada = (char *) malloc(sizeof(char);
+    fgets(entrada, sizeof(entrada),stdin );
+    
+    
+    
+}
